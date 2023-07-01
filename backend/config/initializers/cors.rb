fe6d888374c0fc47fea0ac2ed430bd9b06343073
ahsanaminofficial <amin.ahsan2@gmail.com>
2023-07-01 23:33:07 +0500

    added the devise functionality

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -15,11 +15,11 @@
 #   end
 # end
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#     allow do
-#         origins '*'
-#     resource '*',
-#         headers: :any,
-#         methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#     end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+        origins '*'
+    resource '*',
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    end
+end
